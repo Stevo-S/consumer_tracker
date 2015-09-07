cb8365e18486f16c496fffee762e0101add58c2b
Steve Savio <stevesavio@rocketmail.com>
2015-09-08 01:23:31 +0300

    Changed up the admin dashboard a little bit

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,23 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   sidebar :Categories, priority: 0 do
-    p 'TODO: Add categories'
+    details do
+	summary do
+	    strong 'Categories list'
+	end
+
+	ul do
+	    li 'Electronics'
+	    li 'Mobile Phones'
+	    li 'Sports and Fitness'
+	    li 'Books'
+	    li 'Fashion'
+	    li 'Tablets and Accessories'
+	    li 'Home and Living'
+	    li 'Computing and Accessories'
+	    li 'Baby, Kids and Toys'
+	end
+    end
   end
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
@@ -52,6 +68,11 @@ ActiveAdmin.register_page "Dashboard" do
 	    end
 	end
 	column do
+	    panel "LOGGED IN" do
+		h1 "1"
+	    end
+	end
+	column do
 	    panel "RECEIVED MESSAGES" do
 		h1 IncomingMessage.all.count
 	    end
@@ -61,33 +82,43 @@ ActiveAdmin.register_page "Dashboard" do
     br
 
     columns do
-	column max_width: "320px" do
-	    panel 'PRODUCTS', id: 'chart' do
-		canvas id: 'pie_canvas'
+	column  do
+	    panel 'Categories' do
+		canvas id: 'categories_bar_chart_canvas', height: "350", width: "400"
+		render 'categories_bar_chart'
 	    end
-	    panel 'Top 5 Products' do
-		canvas id: 'bar_chart_canvas'
-		render 'bar_chart'
-	    end
-	    render 'pie_chart'
 	end
 
-	column min_width: "525px" do
+	column  do
 	    panel 'Messages' do
-		canvas id: 'canvas', height: "360", width: "500"
+		canvas id: 'canvas', height: "360", width: "400"
 	    end
 	    render 'line_chart'
 	end
     end # end of row 1
 
     columns do
-	column max_width: 700 do
-	    panel 'Categories' do
-		canvas id: 'categories_bar_chart_canvas', height: "350", width: "600"
-		render 'categories_bar_chart'
+	column  do
+	    panel 'PRODUCTS', id: 'chart' do
+		canvas id: 'pie_canvas'
 	    end
 	end
-    end
 
+	column  do
+	    panel 'Top 5 Products' do
+		canvas id: 'bar_chart_canvas', width: 270
+		render 'bar_chart'
+	    end
+	    render 'pie_chart'
+	end
+
+	column do
+	    panel 'Quarterly Confirmations' do
+		canvas id: 'doughnut_canvas'
+		render 'quarterly_doughnut'
+	    end
+	end
+
+    end # end of row 2
   end # content
 end
